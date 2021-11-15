188ac49d9833248f68e63513c9e84208dfb42a9e
lifd <654321lfd>
2021-11-15 16:57:51 +0800

    子组件给父组件传参

---
+++ src/components/bodyChild.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default class BodyChild extends React.Component{
+  render(){
+    return(
+      <div>
+        子页面输入：<input type="text" onChange={this.props.handleChildValueChange}/>
+      </div>
+    )
+  }
+}
--- src/components/bodyIndex.js
+++ src/components/bodyIndex.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import BodyChild from './bodyChild'
 export default class BodyIndex extends React.Component {
     componentWillMount() {
         console.log("BodyIndex-componentWillMount")
@@ -11,8 +11,12 @@ export default class BodyIndex extends React.Component {
     constructor() {
         super()//调用基类的左右初始化方法
         this.state = { //初始化赋值
-            userName: 'lifangdan'
+            userName: 'lifangdan',
+            age: 20
         }
+    }
+    handleChange(event) {
+        this.setState({ age: event.target.value })
     }
 
     render() {
@@ -20,22 +24,11 @@ export default class BodyIndex extends React.Component {
             //更改state
             this.setState({ userName: 'feizhoumao' })
         }, 3000)
-        var userName = 'lifangdan'
-        var boolInput = true
-        var html = '测试标签'
-        var html1 = '测试  空格显示1'
-        var html2 = '测试\u0020\u0020空格显示2'
         return (
             <div>
                 <h2>页面主体内容</h2>
-                <p>{this.state.userName},{this.props.userId},{this.props.userAge}</p>
-    
-                <p>{userName === '' ? '用户还没有登录' : '用户名：' + userName}</p>
-                <p><input type="button" disabled={boolInput} value={userName} /></p>
-                <p>{html}</p>
-                {/**文本中有空格可以使用dangerouslySetInnerHTML，或者将空格转码展示 */}
-                <p dangerouslySetInnerHTML={{ __html: html1 }}></p>
-                <p>{html2}</p>
+                <p>年龄：{this.state.age}</p>
+                <BodyChild handleChildValueChange={this.handleChange.bind(this)} />
             </div>
         )
     }
