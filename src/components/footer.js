046c1df72f6dc29bb3621abbe80c285558a553e2
lifd <654321lfd>
2021-11-18 17:25:13 +0800

    局部樣式和全局樣式的使用

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import footerStyle from '../css/modules/footer.module.css'
 export default class ComponentFooter extends React.Component {
     render() {
         return (
-            <footer>
+            <footer className={footerStyle.footer}>
                 <h1>这里是页脚</h1>
             </footer>
         )
