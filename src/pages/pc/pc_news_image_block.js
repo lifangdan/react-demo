5610d4b3682594f07b6597a8e5e89b5574244136
lifangdan <845154579@qq.com>
2021-11-26 18:04:16 +0800

    图片模块提交

---
+++ src/pages/pc/pc_news_image_block.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { Card } from 'antd';
+export default class PCNewsImageBlock extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            news: ''
+        }
+    }
+    componentWillMount() {
+        const data1 = [
+            {
+                uniquekey: 1,
+                title: '头条新闻',
+                src: '../images/carousel_1.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 2,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 3,
+                title: '头条新闻',
+                src: '../images/carousel_3.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 4,
+                title: '头条新闻',
+                src: '../images/carousel_4.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 5,
+                title: '头条新闻',
+                src: '../images/carousel_1.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 1,
+                title: '头条新闻',
+                src: '../images/carousel_1.jpg',
+                name: '非洲猫'
+            }
+        ]
+        const data2 = [
+            {
+                uniquekey: 1,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 2,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 3,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 4,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫'
+            },
+            {
+                uniquekey: 5,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫'
+            }
+        ]
+        // if (this.props.type === 'top') {
+        //     this.setState({ news: data1 })
+        // } else if (this.props.type === 'guoji') {
+        //     this.setState({ news: data2 })
+        // }
+        this.setState({ news: data1 })
+    };
+    render() {
+        const styleImage = {
+            display: "block",
+            width: this.props.imageWidth,
+            height: '90px'
+        }
+        const styeH3 = {
+            width: this.props.imageWidth,
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis"
+        };
+        const news = this.state.news
+        const newsList = news.length
+            ? news.map((newsItem, index) => (
+                <div className="imageblock">
+                    <div className="custom-image">
+                        <img style={styleImage} alt='' src={newsItem.src} />
+                    </div>
+                    <div className="custom-card">
+                        <h4 style={styeH3}>{newsItem.title}</h4>
+                        <p>{newsItem.name}</p>
+                    </div>
+                </div>
+            ))
+            : '没有加载到任何新闻';
+        return (
+            <div className="topNewsList">
+                <Card title={this.props.cartTitle} bordered={true} style={{ width: this.props.width }}>
+                    {newsList}
+                </Card>
+
+            </div>
+        )
+    }
+}
