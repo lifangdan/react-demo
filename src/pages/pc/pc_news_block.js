42a78d2d24ee4e1b6591db4dabf6748620afdea7
lifangdan <654321lfd>
2021-11-26 16:36:24 +0800

    添加新闻模块

---
+++ src/pages/pc/pc_news_block.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Link, Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Card } from 'antd';
+
+export default class PCNewsBlock extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            news: ''
+        }
+    };
+    componentWillMount() {
+        const data1 = [
+            {
+                uniquekey: 1,
+                title: '我是头条新闻',
+            },
+            {
+                uniquekey: 2,
+                title: '我是头条新闻',
+            },
+            {
+                uniquekey: 3,
+                title: '我是头条新闻',
+            },
+            {
+                uniquekey: 4,
+                title: '我是头条新闻',
+            },
+            {
+                uniquekey: 5,
+                title: '我是新闻标题',
+            }
+        ]
+        const data2 = [
+            {
+                uniquekey: 1,
+                title: '这里是国际新闻',
+            },
+            {
+                uniquekey: 2,
+                title: '这里是国际新闻',
+            },
+            {
+                uniquekey: 3,
+                title: '这里是国际新闻',
+            },
+            {
+                uniquekey: 4,
+                title: '这里是国际新闻',
+            },
+            {
+                uniquekey: 5,
+                title: '这里是国际新闻',
+            }
+        ]
+        if (this.props.type === 'top') {
+            this.setState({ news: data1 })
+        } else if (this.props.type === 'guoji') {
+            this.setState({ news: data2 })
+        }
+
+    };
+    render() {
+        const news = this.state.news
+        const newsList = news.length
+            ? news.map((newsItem, index) => (
+                <li key={index}>
+                        {newsItem.title}
+                </li>
+            ))
+            : '没有加载到任何新闻';
+        return (
+            <div className="topNewsList">
+                <Card>
+                    <ul>
+                        {newsList}
+                    </ul>
+                </Card>
+            </div>
+        )
+    }
+}
--- src/pages/pc/pc_newscontainer.js
+++ src/pages/pc/pc_newscontainer.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Row, Col, Carousel } from 'antd';
+import { Tabs, Row, Col, Carousel } from 'antd';
+import PCNewsBlock from './pc_news_block';
 
+const TabPane = Tabs.TabPane;
 export default class PCNewsContainer extends React.Component {
     render() {
-        const settings={
-            dots:true,
-            speed:500,
-            autoplay:true,
+        const settings = {
+            dots: true,
+            speed: 500,
+            autoplay: true,
         }
         return (
             <div>
                 <Row>
                     <Col span={2}></Col>
-                    <Col span={20} class="container">
+                    <Col span={20} className="container">
                         <div className="leftContainer">
                             <div className="carousel">
                                 <Carousel {...settings}>
@@ -20,10 +22,17 @@ export default class PCNewsContainer extends React.Component {
                                     <div><img src="../images/carousel_2.jpg" /></div>
                                     <div><img src="../images/carousel_3.jpg" /></div>
                                     <div><img src="../images/carousel_4.jpg" /></div>
-                                    <div><img src="../images/carousel_5.jpg" /></div>
                                 </Carousel>
                             </div>
                         </div>
+                        <Tabs className="tabs_news">
+                            <TabPane tab="头条新闻" key="1">
+                                <PCNewsBlock count={5} type="top" width="100%" bordered="false" />
+                            </TabPane>
+                            <TabPane tab="国际" key="2">
+                                <PCNewsBlock count={5} type="guoji" width="100%" bordered="false" />
+                            </TabPane>
+                        </Tabs>
                     </Col>
                     <Col span={2}></Col>
                 </Row>
