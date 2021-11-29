e13365138a6a25085d0231999a845db828553a02
lifangdan <845154579@qq.com>
2021-11-29 15:29:05 +0800

    移动端添加新闻列表

--- src/pages/mobile/mobile_index.js
+++ src/pages/mobile/mobile_index.js
@@ -1,21 +1,33 @@
 import React from "react";
 import MobileHeader from '../../components/mobile_header'
 import MobileFooter from '../../components/mobile_footer'
+import MobileList from './mobile_list';
 import {
     Tabs,
 } from 'antd';
 const { TabPane } = Tabs;
+
 export default class MobileIndex extends React.Component {
     render() {
         return (
             <div>
                 <MobileHeader />
-                <Tabs>
-                    <TabPane tab="头条" key="1"></TabPane>
-                    <TabPane tab="社会" key="2"></TabPane>
-                    <TabPane tab="国内" key="3"></TabPane>
-                    <TabPane tab="国际" key="4"></TabPane>
-                    <TabPane tab="娱乐" key="5"></TabPane>
+                <Tabs tabBarGutter={50}>
+                    <TabPane tab="头条" key="1">
+                        <MobileList type="top" cartTitle="头条"/>
+                    </TabPane>
+                    <TabPane tab="社会" key="2">
+                    <MobileList type="shehui" cartTitle="社会"/>
+                    </TabPane>
+                    <TabPane tab="国内" key="3">
+                    <MobileList type="guonei" cartTitle="国内"/>
+                    </TabPane>
+                    <TabPane tab="国际" key="4">
+                    <MobileList type="guoji" cartTitle="国际"/>
+                    </TabPane>
+                    <TabPane tab="娱乐" key="5">
+                    <MobileList type="yule" cartTitle="娱乐"/>
+                    </TabPane>
                 </Tabs>
                 <MobileFooter />
             </div>
---
+++ src/pages/mobile/mobile_list.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import { Router, Route, Link, browserHistory } from 'react-router'
+export default class MobileList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            news: ''
+        };
+    }
+    componentWillMount() {
+        const data1 = [
+            {
+                uniquekey: 1,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+
+            },
+            {
+                uniquekey: 2,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+            {
+                uniquekey: 3,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+            {
+                uniquekey: 4,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+            {
+                uniquekey: 5,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+            {
+                uniquekey: 6,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+            {
+                uniquekey: 7,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+            {
+                uniquekey: 8,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+            {
+                uniquekey: 9,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+            {
+                uniquekey: 10,
+                title: '头条新闻',
+                src: '../images/carousel_2.jpg',
+                name: '非洲猫',
+                realtype: '头条',
+                date: '2021-11-29'
+            },
+
+        ]
+        this.setState({ news: data1 })
+    };
+    render() {
+        const { news } = this.state;
+        const newsList = news.length
+            ? news.map((newsItem, index) => (
+                <section key={index} className="m_article list-item special_section clearfix">
+                    <div>
+                        <div className="m_article_img">
+                            <img src={newsItem.src} alt={newsItem.title} />
+                        </div>
+                        <div className="m_article_info">
+                            <div className="m_article_title">
+                                <span>{newsItem.title}</span>
+                            </div>
+                            <div className="m_article_desc clearfix">
+                                <div className="m_article_desc_l">
+                                    <span className="m_article_channel">{this.props.cartTitle}</span>
+                                    <span className="m_article_time">{newsItem.date}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            ))
+            : '没有加载到任何新闻';
+        return (
+            <div>
+                <Row>
+                    <Col span={24}>
+                        {newsList}
+                    </Col>
+                </Row>
+            </div>
+        );
+    };
+}
